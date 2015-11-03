5762820ee17b1f4479aaeb4cb7e132db140a05e9
andrliu <dieseliu321@gmail.com>
2015-11-03 15:30:04 -0800

    first commit

---
+++ ALImagePicker/ImagePickerCropVC.h
@@ -0,0 +1,13 @@
+//
+//  ImagePickerCropVC.h
+//  ALImagePicker
+//
+//  Created by Andrew Liu on 10/26/15.
+//  Copyright © 2015 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagePickerCropVC : UIViewController
+
+@end
